f6b9b8b141605e35477eab34313f70c0675c299b
Kenneth Gorking <kgorking@outlook.com>
2025-02-27 00:19:45 +0100

    Try to trigger analyzer

--- test/src/extra.cpp
+++ test/src/extra.cpp
@@ -1,8 +1,9 @@
 import std;
 //import test.will_it_work;
 
-void extra() {
+short extra(short *p) {
 	std::println("  This is an extra function");
+	return *p;
 	//if (does_it_work())
 	//	std::println("  module works as well");
 }
--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,10 +1,10 @@
 import std;
 
-void extra();
+short extra(short*);
 
 int main() {
 	std::println("Created by the Gorking build system v0.01");
-	extra();
+	return extra(nullptr);
 
 	return 0;
 }
