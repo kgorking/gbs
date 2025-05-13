5ed1e8bbabe3b084b8e38673cfbb37792bcbf666
Kenneth Gorking <kgorking@outlook.com>
2025-05-13 22:01:15 +0200

    Can build in parallel

--- src/build_msvc.cpp
+++ src/build_msvc.cpp
@@ -9,6 +9,49 @@ namespace fs = std::filesystem;
 using namespace std::string_view_literals;
 
 bool build_msvc(context& ctx, std::string_view args) {
+	// Find the source files and dependencies
+	auto file_dependency_map  = std::unordered_map<fs::path, source_dependency>{};
+	auto file_to_imports_map  = std::unordered_map<fs::path, std::set<std::string>>{};
+	auto module_name_to_file_map   = std::unordered_map<std::string, fs::path>{};
+	auto grouped_source_files = std::vector<std::vector<fs::path>>{};
+
+	for (fs::directory_entry it : fs::recursive_directory_iterator("src")) {
+		if (it.is_directory())
+			continue;
+
+		auto const path = it.path();
+		auto const deps = detect_module_dependencies(path);
+		file_dependency_map[path] = deps;
+
+		if (!deps.export_name.empty())
+			module_name_to_file_map[deps.export_name] = path;
+	}
+
+	for (auto const& [path, deps] : file_dependency_map) {
+		auto process_dependencies = [&](this auto& self, fs::path const& path, source_dependency const& deps) -> void {
+			if (file_to_imports_map.contains(path))
+				return;
+
+			file_to_imports_map[path].insert_range(deps.import_names);
+
+			for (auto const& dep : deps.import_names) {
+				if (auto it = module_name_to_file_map.find(dep); it != module_name_to_file_map.end()) {
+					auto const& dep_path = it->second;
+					self(dep_path, file_dependency_map[dep_path]);
+					file_to_imports_map[path].insert_range(file_to_imports_map[dep_path]);
+				}
+			}
+			};
+
+		process_dependencies(path, deps);
+
+		auto const dep_size = file_to_imports_map[path].size();
+		while(grouped_source_files.size() <= dep_size) {
+			grouped_source_files.emplace_back();
+		}
+		grouped_source_files[(int)dep_size].push_back(path);
+	}
+
 	// Build output
 	ctx.output_config = args.substr(0, args.find(','));
 	auto const output_dir = ctx.output_dir();
@@ -59,44 +102,21 @@ bool build_msvc(context& ctx, std::string_view args) {
 		}
 	}
 
-	// Find the source files
-	// Detect dependencies
-	auto source_files = std::unordered_map<fs::path, source_dependency>{};
-	std::unordered_map<std::string, fs::path> module_map;
-
-	for (fs::directory_entry it : fs::recursive_directory_iterator("src")) {
-		if (it.is_directory())
-			continue;
-
-		auto const path = it.path();
-		auto const deps = detect_module_dependencies(path);
-		source_files[path] = deps;
-
-		if (!deps.export_name.empty())
-			module_map[deps.export_name] = path;
-	}
-
-
-	//auto source_is_compiled = std::make_unique<bool[]>(source_files.size());
+	//for (auto const& [i, paths] : grouped_source_files) {
+	//	for (auto path : paths) {
+	//		std::println(" {} - {}", path.generic_string(), i);
+	//	}
+	//}
 
 	// Set up the compiler helper
-	auto compile_cpp = [&](this auto& self, fs::path const& in, source_dependency const& deps, std::set<std::string_view>& refs_set) -> bool {
+	auto compile_cpp = [&](this auto& self, fs::path const& in) -> bool {
 		auto const obj = (output_dir / in.filename()).replace_extension("obj");
 		if (is_file_up_to_date(in, obj)) {
-			refs_set.insert_range(deps.import_names);
 			return true;
 		}
 
-		for (auto const& dep : deps.import_names) {
-			if (auto it = module_map.find(dep); it != module_map.end()) {
-				auto const dep_path = it->second;
-				self(dep_path, source_files[dep_path], refs_set);
-			}
-		}
-		refs_set.insert_range(deps.import_names);
-
 		std::string refs;
-		for(auto ref : refs_set)
+		for(auto ref : file_to_imports_map[in])
 			refs += std::format("/reference {0}={1}/{0}.ifc ", ref, output_dir.generic_string());
 
 		std::string const cmd = std::format("{0} @{2}/INCLUDE /c /interface /Tp\"{1}\" /reference std={2}/std.ifc {3}",
@@ -105,17 +125,13 @@ bool build_msvc(context& ctx, std::string_view args) {
 			output_dir.generic_string(),
 			refs
 		);
-		//std::puts(cmd.c_str());
-		std::println("{} deps on {}", in.generic_string(), refs_set);
 
 		return (0 == std::system(cmd.c_str()));
 		};
 
 	// Compile sources
-	for (auto [path, deps] : source_files) {
-		auto refs = std::set<std::string_view>{};
-		if (!compile_cpp(path, deps, refs))
-			return false;
+	for (auto const& paths : grouped_source_files) {
+		std::for_each(std::execution::par_unseq, paths.begin(), paths.end(), compile_cpp);
 	}
 
 	std::string const executable = fs::current_path().stem().string() + ".exe";
