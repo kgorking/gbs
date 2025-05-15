cf9eb5dfc74f85de1adb4ea44e21c4489b85c49e
Kenneth Gorking <kgorking@outlook.com>
2025-05-15 12:45:23 +0200

    Call entire module chain

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,12 +1,12 @@
 //import std;
 #include <cstdio>
 //#include <print>
-import mod0;
+import mod9;
 
 short extra();
 
 int main() {
 	std::puts("Created by the Gorking build system");
 	extra();
-	MyFunc0();
+	MyFunc9();
 }
