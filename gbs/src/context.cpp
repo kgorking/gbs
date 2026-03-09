85ef423309c43d6553ab5b89f12f089ef832aced
Kenneth Gorking <kgorking@outlook.com>
2026-03-09 16:59:16 +0100

    Static linking

--- gbs/src/context.cpp
+++ gbs/src/context.cpp
@@ -28,8 +28,8 @@ context::context(char const** envp) : env(envp) {
 			"/w14928 "     // illegal copy-initialization; more than one user-defined conversion has been implicitly applied 
 		},
 		{"_shared", "/nologo /EHsc /sdl /std:c++23preview /D_MSVC_STL_HARDENING=1 /D_MSVC_STL_DESTRUCTOR_TOMBSTONES=1"},
-		{"debug",   "/Od /MD"},
-		{"release", "/DNDEBUG /O2 /MD"},
+		{"debug",   "/Od /MT"},
+		{"release", "/DNDEBUG /O2 /MT"},
 		{"analyze", "/external:W0 /external:Ilib /external:anglebrackets /analyze:external- /analyze:WX- /analyze:plugin EspXEngine.dll"}
 	};
 
